5c9dc2ad31bd772ebd1b427a328013a4aaa84d32
andz <aromero@uni-bremen.de>
2014-07-07 11:17:47 +0200

    Implemented border patrol.

--- fetch_my_ball/src/fetch-my-ball.lisp
+++ fetch_my_ball/src/fetch-my-ball.lisp
@@ -1,6 +1,8 @@
 (in-package :fetch-my-ball)
 
 (setf *wheelbase* 11.5)
+(defvar *border-patrol-subscriber*)
+(setf *suspected-crossing* nil)
 
 (defun init-driver ()
   (roslisp-utilities:startup-ros)
@@ -48,3 +50,39 @@
          (effort-b (+ 0.6 (if (> ratio-ab 1.0) 0.2 (* 0.2 ratio-ab)))))
     (format t "~a ~a" effort-a effort-b)
     (send-joint-commands '("l_motor_joint" "r_motor_joint") `(,effort-a ,effort-b))))
+
+(defun border-patrol (msg)
+  (with-fields ((r r)
+                (g g)
+                (b b))
+      msg
+    ;;(format t "r: ~a g: ~a b: ~a~%" r g b)
+    (if (not (and (= r 0) (= g 0) (= b 0)))
+        (progn
+          ;;(format t "Not on black surface?~%")
+          (if (not *suspected-crossing*)
+              ;; first crossing detected
+              (progn
+                (format t "Detected first possible crossing.~%")
+                (setf *suspected-crossing* (ros-time)))
+              ;; crossing has been detected before
+              (progn
+                (let* ((now (ros-time))
+                       (time-passed (- now *suspected-crossing*)))
+                  (if (> time-passed 0.5)
+                      (format t "Crossing!~%"))))))
+        (if *suspected-crossing*
+            (progn
+              (format t "~a Was no crossing at all.~%" *suspected-crossing*)
+              (setf *suspected-crossing* nil))))))
+
+
+(defun init-border-patrol ()
+  (setf *border-patrol-subscriber*
+        (subscribe "/color_sensor" "nxt_msgs/Color" #'border-patrol))
+  (format t "Subscribed to color sensor.~%"))
+
+(defun order-back-border-patrol ()
+  (unless (eq *border-patrol-subscriber* nil)
+    (unsubscribe *border-patrol-subscriber*)
+    (format t "Unsubscribed color sensor.~%")))
